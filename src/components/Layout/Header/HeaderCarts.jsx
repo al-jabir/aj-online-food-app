6974503e1c0755343620aaf94a4c47f5fe6c6c2c
MD AL JABIR <aljabir098@gmail.com>
2022-09-15 12:27:30 +0600

    cartButton complete

---
+++ src/components/Layout/Header/HeaderCarts.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import CartIcon from "../../Cart/CartIcon";
+
+import styled from "./HeaderCarts.module.css";
+
+const HeaderCarts = () => {
+  return (
+    <button className={styled.button}>
+      <span className={styled.icon}>
+        <CartIcon />
+      </span>
+      <span className={styled.badge}>Your Cart</span>
+      <span>3</span>
+    </button>
+  );
+};
+
+export default HeaderCarts;
