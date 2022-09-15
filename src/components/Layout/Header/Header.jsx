6b6f623439c94ae994313451c5de5f7e6802507e
MD AL JABIR <aljabir098@gmail.com>
2022-09-15 12:06:47 +0600

    add style

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
+import mealsImg from "../../assets/foodmeals.jpg";
+import styled from "./header.module.css";
 const Header = () => {
   return (
     <>
-      <header>
+      <header className={styled.header}>
         <h1>FoodMeals</h1>
         <button>card</button>
       </header>
-      <div>
-        <img alt="logo" />
+      <div className={styled["main-image"]}>
+        <img src={mealsImg} alt="food-meals" />
       </div>
     </>
   );
