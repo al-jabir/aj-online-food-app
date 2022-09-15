1f11faf369a476fd1e004a02d6f498660f2b6305
MD AL JABIR <aljabir098@gmail.com>
2022-09-15 12:16:16 +0600

    some update

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import mealsImg from "../../assets/foodmeals.jpg";
 import styled from "./header.module.css";
 const Header = () => {
   return (
-    <>
-      <header className={styled.header}>
+    <Fragment>
+      <header className={styled.headers}>
         <h1>FoodMeals</h1>
         <button>card</button>
       </header>
       <div className={styled["main-image"]}>
         <img src={mealsImg} alt="food-meals" />
       </div>
-    </>
+    </Fragment>
   );
 };
 
