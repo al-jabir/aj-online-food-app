248fd46d40b41285d24bc4ab40edc51d72b84291
MD AL JABIR <aljabir098@gmail.com>
2022-09-15 14:57:19 +0600

    meals form

---
+++ src/components/Meals/MealsItem/MealsForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "./MealsForm.module.css";
+const MealsForm = () => {
+  return (
+    <form className={styled.form}>
+      <input />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealsForm;
