da5f07ff5af4634af685aa54a4fd27cc352358fd
MD AL JABIR <aljabir098@gmail.com>
2022-09-15 11:54:37 +0600

    header section

---
+++ src/components/Layout/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>FoodMeals</h1>
+        <button>card</button>
+      </header>
+      <div>
+        <img alt="logo" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
