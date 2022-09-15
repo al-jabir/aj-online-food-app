64459f833d598efc5fb350268c533674716fe75d
MD AL JABIR <aljabir098@gmail.com>
2022-09-15 18:59:39 +0600

    modal

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import styled from "./Modal.module.css";
 
-const Modal = () => {
-  return <div>Modal</div>;
+const Backdrop = () => {
+  return <div className={styled.backdrop} />;
+};
+
+const ModalOverlay = ({ children }) => {
+  return (
+    <div className={styled.modal}>
+      <div className={styled.content}>{children}</div>
+    </div>
+  );
+};
+
+const Modal = ({ children }) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop />,
+        document.getElementById("root-modal")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{children}</ModalOverlay>,
+        document.getElementById("root-modal")
+      )}
+    </>
+  );
 };
 
 export default Modal;
