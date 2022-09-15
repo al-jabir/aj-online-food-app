6fa42d20f828590935064bdbe6528c54e3e27325
MD AL JABIR <aljabir098@gmail.com>
2022-09-15 14:22:18 +0600

    done

---
+++ src/components/Meals/MealsSemmary.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from './MealsSummary.module.css'
+
+const MealsSemmary = () => {
+  return (
+    <section className={styled.summary}>
+    <h2>Delicious Food, Deliverd To You</h2>
+    <p>
+    Chose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.
+    </p>
+    <p>
+    All our meals are cooked with high-quality ingredients, just-in-time and of course by experienced chefs!
+    </p>
+    </section>
+  )
+}
+
+export default MealsSemmary
